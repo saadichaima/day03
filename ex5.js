80bedf45c57d64c6b31cca6057557fc5cf44fd4b
saadichaima <99804707+saadichaima@users.noreply.github.com>
2024-10-11 10:05:04 +0200

    Update

---
+++ ex5.js
@@ -0,0 +1,10 @@
+function getHousesPrototype() {
+    const map = new Map(); 
+     map.set('Apartment 75008', 90);
+     map.set('Apartment 94300', 105);
+     map.set('Apartment 92300', 140);
+     map.set('Apartment 75015', 65);
+ 
+ 
+     return map; 
+ }
