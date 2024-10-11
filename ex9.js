3633c929ae45c5ebd810d3756238da655bb3defc
saadichaima <99804707+saadichaima@users.noreply.github.com>
2024-10-11 10:06:11 +0200

    Update

---
+++ ex9.js
@@ -0,0 +1,16 @@
+// Write your code here...
+
+function addHousing(housingList, newHouse, quantity) {
+    if (housingList.has(newHouse)) {
+        housingList.set(newHouse, housingList.get(newHouse) + quantity);
+    } else {
+         housingList.set(newHouse, quantity);
+    }
+}
+let housingList = new Map();
+addHousing(housingList, "Flat", 2);
+addHousing(housingList, "Log", 3);
+addHousing(housingList, "Flat", 3);
+addHousing(housingList, "Hutch", 2);
+
+displayResult(housingList);
